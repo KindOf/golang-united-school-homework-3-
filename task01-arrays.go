7955a6105e1fb99ccdd0b812a41e49d4119178aa
ihor_ostapovych <thor.ostapovych@danfoss.com>
2022-05-13 15:12:05 +0300

    feat(task01-arrays.go): implemented

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,12 @@
 package homework
 
-func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+func average(input [6]float32) (result float32) {
+	var sum float32
+	var l int
+
+	for _, n := range input {
+		l++
+		sum += n
+	}
+	return sum / float32(l)
 }
---
+++ task01-arrays_test.go
@@ -0,0 +1,24 @@
+package homework
+
+import "testing"
+
+func Test_average(t *testing.T) {
+	tests := []struct {
+		name  string
+		input [6]float32
+		want  float32
+	}{
+		{
+			name:  "Should return avarage 3.5",
+			input: [6]float32{1, 2, 3, 4, 5, 6},
+			want:  3.5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotResult := average(tt.input); gotResult != tt.want {
+				t.Errorf("average() = %v, want %v", gotResult, tt.want)
+			}
+		})
+	}
+}
